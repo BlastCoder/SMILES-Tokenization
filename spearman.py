e0a0a128a3e91fadb429105a6b495f9a95c075d3
Arvind Venkateshr <git@rvind.dev>
2025-10-28 20:52:32 -0700

    feat: plot for correlations

--- spearman.py
+++ spearman.py
@@ -6,12 +6,16 @@ from utils import iter_smiles
 from rdkit import Chem, DataStructs
 from rdkit.Chem import AllChem
 from operator import mul
+import matplotlib
+import matplotlib.pyplot as plt
+
+# matplotlib.use('webagg') # Install `tornado` for web backend
 
 SLICE = "data/chembl_test.parquet"
 STATE = "trie_ttg_chembl.pkl"
 TRAIN = "data/chembl_train_100K.parquet"
 
-cap = 100000
+cap = 50000
 
 fpgen = AllChem.GetMorganGenerator(radius=2)
 
@@ -48,7 +52,7 @@ def main():
     for i in range(len(z)):
         if n >= cap: break
         for j in range(len(z)):
-            if i >= j: continue
+            if i > j: continue
             if n >= cap: break
             cos = sum(map(mul, z[i], z[j])) / (sum(map(lambda x: x**2, z[i]))**0.5 * sum(map(lambda x: x**2, z[j]))**0.5)
             tanimoto = DataStructs.TanimotoSimilarity(ecfp[i], ecfp[j])
@@ -60,11 +64,19 @@ def main():
     rank_ecfp = sorted(d, key=lambda p: p[3])
 
     d2_i = []
+    xy = []
 
     for i in range(len(rank_ttg)):
         d2_i.append((i - rank_ecfp.index(rank_ttg[i]))**2)
+        xy.append((i, rank_ecfp.index(rank_ttg[i])))
 
     print(f"p = {1 - ((6 * sum(d2_i))/(len(rank_ttg) * (len(rank_ttg)**2 - 1)))}")
+
+    plt.scatter(*zip(*xy))
+    plt.xlabel("Rank for TTG Similarity")
+    plt.ylabel("Rank for ECFP Similarity")
+    plt.show()
+    
     
 if __name__ == "__main__":
     main()

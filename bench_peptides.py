0391f3f07a66286b3a88d7c754824650d9efb3c9
Arvind Venkateshr <git@rvind.dev>
2025-10-30 23:00:04 -0700

    feat: results from e7, e1

---
+++ bench_peptides.py
@@ -0,0 +1,130 @@
+# bench_lengths.py
+from utils import iter_smiles
+from SmilesPE.tokenizer import *
+from collections import Counter
+import trie_funcs as tf
+import numpy as np
+import codecs
+import tqdm
+import time
+import math
+import json
+
+SLICE = "data/peptide/peptides_1M.parquet"
+APE_DIR = "ape_peptide"  # 48452.2s for training
+TRIE_FILE = "./trie_ttg_peptide.pkl"  # 12.9s for training
+TRIE_TTG_FILE = "trie_ttg_peptide.pkl"  # 11.6s for training
+SPE_FILE = "spe_peptide.txt"  # 82.5s for training
+
+def is_outlier(points, thresh=3.5):
+    """
+    Returns a boolean array with True if points are outliers and False 
+    otherwise.
+
+    Parameters:
+    -----------
+        points : An numobservations by numdimensions array of observations
+        thresh : The modified z-score to use as a threshold. Observations with
+            a modified z-score (based on the median absolute deviation) greater
+            than this value will be classified as outliers.
+
+    Returns:
+    --------
+        mask : A numobservations-length boolean array.
+
+    References:
+    ----------
+        Boris Iglewicz and David Hoaglin (1993), "Volume 16: How to Detect and
+        Handle Outliers", The ASQC Basic References in Quality Control:
+        Statistical Techniques, Edward F. Mykytka, Ph.D., Editor. 
+    """
+    if len(points.shape) == 1:
+        points = points[:, None]
+    median = np.median(points, axis=0)
+    diff = np.sum((points - median)**2, axis=-1)
+    diff = np.sqrt(diff)
+    med_abs_deviation = np.median(diff)
+
+    modified_z_score = 0.6745 * diff / med_abs_deviation
+
+    return modified_z_score > thresh
+
+def mean_len(generator, fn, desc):
+    tot = n = 0
+    for s in tqdm.tqdm(generator, desc=desc):
+        tot += fn(s)
+        n += 1
+    return tot / n
+    
+
+def var_len(generator, fn, desc, mean):
+    tot = n = 0
+    for s in tqdm.tqdm(generator, desc=desc):
+        tot += (fn(s) - mean) ** 2
+        n += 1
+    return tot / n
+    
+    
+def entropy(mols, fn):
+    token_freq = Counter()
+    tot_tokens = 0
+    for mol in mols:
+        tokens = fn(mol)
+        token_freq.update(tokens)
+        tot_tokens += len(tokens)
+    
+    entropy = 0
+    for count in token_freq.values():
+        p = count / tot_tokens
+        entropy -= p * math.log2(p)
+    return entropy
+    
+    
+def main():
+    trie_ttg_state = tf.load_state(TRIE_TTG_FILE)
+    spe_vocab = codecs.open(SPE_FILE)
+    spe = SPE_Tokenizer(spe_vocab)
+
+    t0 = time.time()
+
+    trie_ttg_avg = mean_len(iter_smiles(SLICE),
+                            lambda s: tf.compress_and_len(s, trie_ttg_state),
+                            "Trie-TTG ")
+    trie_ttg_var = var_len(iter_smiles(SLICE),
+                            lambda s: tf.compress_and_len(s, trie_ttg_state),
+                            "Trie-TTG variance  ",
+                            trie_ttg_avg)
+    trie_ttg_entropy = entropy(list(iter_smiles(SLICE)),
+                               lambda s: tf.compress_and_return(s, trie_ttg_state))
+    spe_avg = mean_len(iter_smiles(SLICE),
+                       lambda s: len(spe.tokenize(s).split(" ")),
+                       "SPE  ")
+    spe_var = var_len(iter_smiles(SLICE),
+                      lambda s: len(spe.tokenize(s).split(" ")),
+                      "SPE variance  ",
+                      spe_avg)
+    spe_entropy = entropy(list(iter_smiles(SLICE)),
+                          lambda s: spe.tokenize(s).split(" "))
+    
+    print(f"SPE mean tokens : {spe_avg:5.2f}")
+    print(f"SPE variance in tokens/mol : {spe_var:5.2f}")
+    print(f"SPE entropy in bits : {spe_entropy:5.2f}")
+    print(f"Trie+TTG mean tokens : {trie_ttg_avg:5.2f}")
+    print(f"Trie+TTG variance in tokens/mol : {trie_ttg_var:5.2f}")
+    print(f"Trie+TTG entropy in bits : {trie_ttg_entropy:5.2f}")
+
+    len_orig = 0
+    len_trie = 0
+    len_spe = 0
+    
+    for x in tqdm.tqdm(iter_smiles(SLICE), desc="Compression Ratio"):
+        len_trie += tf.compress_and_len(x, trie_state)
+        len_spe += len(spe.tokenize(x).split(" "))
+        len_orig += len(tf.tokenize(x))
+        
+    print(f"Compression Ratio - Trie {len_orig/len_trie}")
+
+    print(f"Compression Ratio - SPE {len_orig/len_spe}")
+
+if __name__ == "__main__":
+    main()
--- make_slice.py
+++ make_slice.py
@@ -1,17 +1,17 @@
 # make_slice.py
 import pyarrow.dataset as ds, pyarrow as pa, pyarrow.parquet as pq, os
 
-SRC_DIR = "data/chembl"                           # the 5-shard directory
-DST_FN  = "data/chembl_train_100K.parquet"        # 100K-row slice to create
+SRC_DIR = "data/peptide/peptides_10M.parquet"                           # the 5-shard directory
+DST_FN  = "data/peptide/peptides_1M.parquet"        # 100K-row slice to create
 
 def main():
     rows = []
     for batch in ds.dataset(SRC_DIR, format="parquet")\
                   .scanner(columns=["SMILES"]).to_batches():
         rows.extend(batch.column("SMILES").to_pylist())
-        if len(rows) >= 100000:
+        if len(rows) >= 1100000:
             break
-    pq.write_table(pa.Table.from_pydict({"SMILES": rows[:100000]}), DST_FN)
+    pq.write_table(pa.Table.from_pydict({"SMILES": rows[100000:1100000]}), DST_FN)
     print("✔ wrote", DST_FN)
 
 if __name__ == "__main__":
